320a234dd317271f64875485ac212e000157bcc8
araujo0608 <68195900+araujo0608@users.noreply.github.com>
2021-11-02 09:58:57 -0300

    Finishing Viewer App
    Finishing the viewer app images

--- building/image viewer app/viewer.py
+++ building/image viewer app/viewer.py
@@ -3,7 +3,7 @@ from PIL import ImageTk, Image
 import emoji
 
 root = Tk()
-#root.iconbitmap('images/icon python.ico') #defining icon
+root.iconbitmap('images/icon python.ico') #defining icon
 
 root.title('Viewer App') #defining title
 
@@ -16,26 +16,57 @@ img5 = ImageTk.PhotoImage(Image.open('images/nature5.jpg'))
 img6 = ImageTk.PhotoImage(Image.open('images/nature6.jpg'))
 
 image_list = [img1, img2, img3, img4, img5, img6] #index: 0 to 5
-
+index = 0
 
 #label to show image
-lblImage = Label(image=img1)
+lblImage = Label(image=image_list[index])
 lblImage.grid(row=0, column=0, columnspan=3)
 
 
 #Functions
-def forward():
-	pass
+def back(indexn):
+	global index
+	global bntBack
+	global lblImage
+
+	if indexn == 0:
+		index = 5
+		lblImage.grid_forget()
+		lblImage = Label(image=image_list[index])
+		lblImage.grid(row=0, column=0, columnspan=3)
+	else:
+		index = index - 1
+		lblImage.grid_forget()
+		lblImage = Label(image=image_list[index])
+		lblImage.grid(row=0, column=0, columnspan=3)
 
 
-def back():
-	pass
+
+
+
+def forward(indexn):
+
+	global index
+	global btnNext
+	global lblImage
+
+	if indexn == 5:
+		index = 0
+		lblImage.grid_forget()
+		lblImage = Label(image=image_list[index])
+		lblImage.grid(row=0, column=0, columnspan=3)
+	else:
+		index = index + 1
+		lblImage.grid_forget()
+		lblImage = Label(image=image_list[index])
+		lblImage.grid(row=0, column=0, columnspan=3)
+
 
 
 #Buttons (back, exit, next)
-btnBack = Button(root, text=emoji.emojize(':arrow_left:', use_aliases=True), font=10, command=back)
+btnBack = Button(root, text=emoji.emojize(':arrow_left:', use_aliases=True), font=10, command=lambda:back(index))
 btnExit = Button(root, text='EXIT', font=10, command=root.quit)
-btnNext = Button(root, text=emoji.emojize(':arrow_right:', use_aliases=True), font=10, command=forward)
+btnNext = Button(root, text=emoji.emojize(':arrow_right:', use_aliases=True), font=10, command=lambda:forward(index))
 
 
 #Puting the button on screen
