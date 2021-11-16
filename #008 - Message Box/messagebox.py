b5138c7ae6b88e02f0cc7ef3bdf5b015de90d004
araujo0608 <68195900+araujo0608@users.noreply.github.com>
2021-11-15 21:58:34 -0300

    Update messagebox.py
    Just update

--- #008 - Message Box/messagebox.py
+++ #008 - Message Box/messagebox.py
@@ -1,4 +1,12 @@
 from tkinter import *
+from tkinter import messagebox as msgbox
+
+
 root = Tk()
 root.title('Message Box')
+
+
+r = msgbox.askquestion("Error", "Hey, you want delete c:/System32 path?")
+lblR = Label(root, text=r).pack()
+
 root.mainloop()
